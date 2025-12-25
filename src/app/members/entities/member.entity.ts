00e1dcd74ca31e99f9da10e210ab883cdc6c7632
Flourence <web3lotttery143@gmail.com>
2025-12-25 22:49:20 +0800

    update: return value of bets

--- src/app/members/entities/member.entity.ts
+++ src/app/members/entities/member.entity.ts
@@ -3,11 +3,15 @@ export class Member {
     upline_address: String;
 }
 
-export class MemberBets {
-    member_address: String;
+export class Bet {
     bet_number: String;
     bet_amount: String;
     transaction_hash: String;
-    date: String;
+    date: Date;
     draw_number: String;
+}
+
+export class MemberBets {
+    member_address: String;
+    bets: Bet[];
 }
