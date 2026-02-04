4431a866f874af7a095a136c60933b8e00ab4c92
Flourence <web3lotttery143@gmail.com>
2026-02-04 11:21:59 +0800

    add: admin api route

---
+++ src/app/admin/admin.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Admin')
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+ @Get('member-bets')
+  findBets() {
+    return this.adminService.findBets();
+  }
+}
---
+++ src/app/admin/admin.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+import { MEMBER_BETS_MODEL } from 'src/database/constants/constants';
+import { Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { MemberBets } from '../members/entities/member.entity';
+
+@Injectable()
+export class AdminService {
+  constructor(
+    @Inject(MEMBER_BETS_MODEL) private memberBetsModel: Model<MemberBets>
+  ) {}
+
+  async findBets() {
+    return await this.memberBetsModel.aggregate([
+      {
+    $unwind: '$bets'
+      },
+      {
+        $match: {
+          'bets.success': true
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          member_address: 1,
+          bet_number: '$bets.bet_number',
+          date: '$bets.date'
+        }
+      }
+    ])
+  }
+}
